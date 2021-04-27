b31db840a4b204077494ffb1640366146b3d8c59
ahmedbilal2502 <40793135+ahmedbilal2502@users.noreply.github.com>
2021-04-27 10:29:28 +0500

    Update NoteController.java

--- src/main/java/com/example/easynotes/controller/NoteController.java
+++ src/main/java/com/example/easynotes/controller/NoteController.java
@@ -27,34 +27,4 @@ public class NoteController {
     public Note createNote(@Valid @RequestBody Note note) {
         return noteRepository.save(note);
     }
-
-    @GetMapping("/notes/{id}")
-    public Note getNoteById(@PathVariable(value = "id") Long noteId) {
-        return noteRepository.findById(noteId)
-                .orElseThrow(null);
-    }
-
-    @PutMapping("/notes/{id}")
-    public Note updateNote(@PathVariable(value = "id") Long noteId,
-                                           @Valid @RequestBody Note noteDetails) {
-
-        Note note = noteRepository.findById(noteId)
-                .orElseThrow(null);
-
-        note.setTitle(noteDetails.getTitle());
-        note.setContent(noteDetails.getContent());
-
-        Note updatedNote = noteRepository.save(note);
-        return updatedNote;
-    }
-
-    @DeleteMapping("/notes/{id}")
-    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
-        Note note = noteRepository.findById(noteId)
-                .orElseThrow(null);
-
-        noteRepository.delete(note);
-
-        return ResponseEntity.ok().build();
-    }
 }
